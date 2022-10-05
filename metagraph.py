9d38dd4546732a784a12c58a59ea051242a08fd6
wyatt-joyner-tg <wyatt.joyner@tigergraph.com>
2022-10-05 19:05:39 +0100

    add(metagraph): add some hard pseudocoded rules

--- metagraph.py
+++ metagraph.py
@@ -1,10 +1,5 @@
 from graph import Graph, Rule
 
-# object which checks groups, AND within groups, OR outside groups
-
-# if tgt_set is a dict...
-#
-
 def check_member(src, tgt_set):
     # src is a list of sets or a single set, tgt_set is always one set
     if type(src) is list:
@@ -16,7 +11,7 @@ def check_member(src, tgt_set):
 
 # if list, get union of
 # if set, get intersection of members of fetch_op
-def get_members(src, tgt_dict, fetch_op=lambda x,y : x.get(y,None)):
+def get_member(src, tgt_dict, fetch_op=lambda x,y : x.get(y,None)):
     if type(src) is list:
         return set.union([set.intersection([fetch_op(tgt_dict, src_elem) for src_set in src for src_elem in src_set])])
     elif type(src) is set:
@@ -26,26 +21,36 @@ def get_members(src, tgt_dict, fetch_op=lambda x,y : x.get(y,None)):
 
 def get_vertex_set(context, vertex, member_edge, group_edge):
     result = None
-    v_set = context.graph.vertices[vertex.id].in_edges.edgetype_to_id[member_edge]
+    v_set = vertex.in_edges.edgetype_to_id[member_edge]
     if len(v_set) > 0:
         result = set(v_set)
     else:
-        v_set = context.graph.vertices[vertex.id].in_edges.edgetype_to_id[group_edge]
+        v_set = vertex.in_edges.edgetype_to_id[group_edge]
         result = list()
         for v_group in v_set:
             result.append(set(context.graph.vertices[v_group].in_edges.edgetype_to_id[member_edge]))
     return result
 
-
+# PSEUDO
+# get a vertex, and get all the parents it is. this is just a set
+# check member of this set
 def check_member_is(context, src, tgt_set):
     return True
 
+# PSEUDO
+# get the most recent context of involved_edge relation,
+# if instance BFS up is until it is a context
 def get_current_context(context, vertex, involved_edge):
     return ""
 
+# PSEUDO
+# this needs to be a very flexible pattern matching thing
+# def BFS
+
 class ActionRule(Rule):
     def __init__(self, context, vertex):
-        self.ego = vertex.in_edges.edgetype_to_id("Is_Ego")
+        self.ego = vertex.in_edges.edgetype_to_id["Is_Ego"]
+        self.use_type = vertex.in_edges.edgetype_to_id["Use_Rule"].keys()
 
     def transform(self, context, target_set: dict, allow: bool):
         return target_set, allow
@@ -63,7 +68,7 @@ class r_Allow(ActionRule):
 
     def __init__(self, context, vertex):
         super().__init__(context, vertex)
-        self.allow_type = get_vertex_set(context, vertex, "Allow_Type", "Allow_group")
+        self.allow_type = get_vertex_set(context, vertex, "Allow_Type", "Allow_Group")
 
     def transform(self, context, target_set: dict, allow: bool):
         return target_set, check_member(self.allow_type, self.ego.out_edges.edgetype_to_id["Is"].keys())
@@ -74,10 +79,10 @@ class r_AllowIndividual(ActionRule):
 
     def __init__(self, context, vertex):
         super().__init__(context, vertex)
-        self.allow_type = get_vertex_set(context, vertex, "Allow_type", "Allow_Group")
+        self.allow_type = get_vertex_set(context, vertex, "Allow_Type", "Allow_Group")
 
     def transform(self, context, target_set: dict, allow: bool):
-        allow_set = get_members(allow_type, context.graph.vertices, fetch_op=lambda x,y : x[y].edgetype_to_id["Is"].keys())
+        allow_set = get_member(allow_type, context.graph.vertices, fetch_op=lambda x,y : x[y].out_edges.edgetype_to_id["Is"].keys())
         target_set["allow"] = target_set["allow"].union(allow_set)
         target_set.remove(self.ego)
         return target_set, allow
@@ -94,7 +99,7 @@ class r_SameContext(ActionRule):
     def transform(self, context, target_set: dict, allow: bool):
         most_recent_context = get_current_context(context, self.ego, "Participant")
         allow = len(self.allow_context) == 0 or check_member(self.allow_context, most_recent_context)
-        allow = allow and not check_member_is(self.allow_context, most_recent_context)
+        allow = allow and not check_member_is(self.disallow_context, most_recent_context)
         if allow == False:
             return target_set, allow
         allow_set = set([p for p in target_set["allow"] if get_current_context(context, p, "Participant") == most_recent_context])
@@ -102,37 +107,49 @@ class r_SameContext(ActionRule):
         target_set["allow"] = allow_set
         return target_set, allow
 
-"""
-metagraph vertices contain a reference to a class.
-when they add an instance, they instantiate that instance 'on' the instance vertex
 
-if ego in disallow context, disallow
-if ego and other in same context, allow other, else disallow other
+# PSEUDO
+class PassActionRule(ActionRule):
+    def __init__(self, context, vertex):
+        super().__init__(context, vertex)
+        # go from use_type to BFS down the Is tree...
+        # instantiate new rule instances and vertices and edges...
+        # for vertex_id in vertex.in_edges.edgetype_to_id["Is"].keys():
+            # self.__class__(context, context.graph.vertices[vertex_id])
 
-NOTE: 'in context' check involves getting most recent edge
-"""
+# PSEUDO
+class r_ResponseRule(PassActionRule):
 
-class r_ResponseConversationAction(ActionRule):
-    # PASS RULE TO ROOT ON ADD
-    # for each action last timestep that is of a type that 'CanRespond'
-    #   if not responded to by self and within some time threshold, allow
-    #   else disallow
-    pass
+    def __init__(self, context, vertex):
+        super().__init__(context, vertex)
+        # go from use_type to actions via CanRespond
+        #self.can_respond_type = vertex.in_edges.edgetype_to_id["Use_Rule"].keys()
+
+    def transform(self, context, target_set: dict, allow: bool):
+        # go to can respond set, go to instances
+        # for each that are within timeframe
+        # put in temp allow set
+        # go from action to instances
+        # go to responded, remove from temp allow set, add to disallow set
+        return target_set, allow
+
+# PSEUDO
+class r_UniqueRule(PassActionRule):
+    def __init__(self, context, vertex):
+        super().__init__(context, vertex)
+        # all the types that count as unique to this rule/vertex
+        # go from use_type to all As_Unique actions
+        # self.unique_type
+
+    def transform(self, context, target_set: dict, allow: bool):
+        # go to all actions of use_type where ego is source
+        # go to all actions of use_type where allowed person is target
+        # subtract sets
+        return target_set, allow
 
 
-class r_ConversationAction(ActionRule):
-
-    # if self in combat, disallow
-    # for each allowed Person
-    #   get self Participation ConversationContext (if none, fine)
-    #   check last participation
-    #   if ConversationContext and not same as self participation
-    #       disallow for this person
-    #   else allow for this person
-    pass
-
-
-
+# TODO: metagraph vertices traverse to instances, which instantiate rule instances on the target vertices.
+# TODO: rules need to register in some event registry
 def generate_metagraph():
     import sys
     import inspect
@@ -142,8 +159,6 @@ generate_metagraph()
 #metagraph = Graph()
 #metagraph.load_json("./metagraph.json")
 
-"""
-"""
 
 """
 SUBJECTIVE BRAIN ACTION VIABILITY RULES
@@ -168,10 +183,12 @@ RespondTag and ShareTag block each other
 alternatively you just have a limit... greet limit = 1
 CanRespond
 
+done
 class ActionRule(Rule):
     def __call__(self, target_set: dict, allow: bool):
         pass
 
+done
 class r_Action(ActionRule):
     # if self is person, allow
     # else disallow
@@ -179,11 +196,13 @@ class r_Action(ActionRule):
     # passing a set that diminishes each time
     pass
 
+done
 class r_InteractionAction(ActionRule):
     # get instance from Person not self
     # allow interaction action
     pass
 
+done
 class r_ConversationAction(ActionRule):
     # if self in combat, disallow
     # for each allowed Person
@@ -194,6 +213,7 @@ class r_ConversationAction(ActionRule):
     #   else allow for this person
     pass
 
+pseudo
 class r_ResponseConversationAction(ActionRule):
     # PASS RULE TO ROOT ON ADD
     # for each action last timestep that is of a type that 'CanRespond'
@@ -201,6 +221,7 @@ class r_ResponseConversationAction(ActionRule):
     #   else disallow
     pass
 
+pseudo
 class r_UniqueConversationAction(ActionRule):
     # PASS RULE TO ROOT ON ADD
     # for each allowed Person
@@ -208,19 +229,20 @@ class r_UniqueConversationAction(ActionRule):
     # else disallow
     pass
 
+doin
 class r_FriendlyConversationAction(ActionRule):
     # for each allowed Person
     # check last relationship between self and other
-    # if not HostileRelationship, allowl
+    # if not HostileRelationship, allow
     # else disallow
     pass
 
 # CombatAction doesn't have a rule
 
 class r_Greet(ActionRule):
-    # TRACK which people have been disallowed (this should be a passed rule, I think)
     # for each allowed Person
-    # if self and person in same non-combat context, disallow
+    # if acknowledged edge... disallow person
+    # if self and person in same non-combat context ADD ACKNOWLEDGED EDGE
     pass
 
 # AskTags doesn't have a rule
