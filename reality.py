0fa1f65e0f51ef6c7c9660f93fcab4bc216b4108
SimonMarkWhittle <simonmarkwhittle@gmail.com>
2022-12-18 21:54:14 -0500

    corrected shortcut tag case

--- reality.py
+++ reality.py
@@ -62,7 +62,7 @@ class SubjectiveReality(Reality):
     def receive_event(self, event: GraphEvent):
 
         event.subgraph["all_edges"] = [
-            edge for edge in event.subgraph["all_edges"] if "shortcut" in edge.edge_type
+            edge for edge in event.subgraph["all_edges"] if "Shortcut" in edge.edge_type
         ]
 
         super().receive_event(event)
