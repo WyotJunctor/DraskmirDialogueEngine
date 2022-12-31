cb1d3cb73645c63fe1515c65f1909326a7a3da9f
Wyatt Joyner <wjoyner127@gmail.com>
2022-12-30 19:40:41 -0500

    bleh 2

--- action_rules.py
+++ action_rules.py
@@ -104,7 +104,7 @@ class ActionRule:
                 if tgt_ref["alias"] in context:
                     tgt_set = context.get(tgt_ref["alias"], set()) & get_set(edge_map.edgetype_to_vertex, edge["type"])
                 else:
-                    tgt_set = context.get(get_set(edge_map.edgetype_to_vertex, edge["type"]))
+                    tgt_set = edge_map.edgetype_to_vertex.get(edge["type"])
 
             tgt_set = self.check_relationships(graph, context, tgt_set, tgt_ref)
 
